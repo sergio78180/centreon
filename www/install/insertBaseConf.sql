1c3cd963bb57c6ff7df2413e270846c352dc66a9
Lionel Assepo <lassepo@merethis.com>
2013-07-24 17:35:03 +0200

    Add new version

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -3,7 +3,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '2.4.4');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '2.4.5');
 
 --
 -- Contenu de la table `contact`
