6f8d1240945dbc1dd9a912d9d201c5cc798a4aad
Lionel Assepo <lassepo@merethis.com>
2013-07-24 18:14:35 +0200

    Remove use_check_result_path

--- www/include/configuration/configGenerate/genMainFile.php
+++ www/include/configuration/configGenerate/genMainFile.php
@@ -148,12 +148,7 @@
 					$key = str_replace("nagios", "icinga", $key);
 				}
 				$str .= $key."=".$value."\n";
-			} else if (($key == "use_check_result_path" || $key == "use_check_result_path") && $value)	{
-				if (isset($tab['monitoring_engine']) && ($tab["monitoring_engine"] == "CENGINE")) {
-					$str .= $key."=".$value."\n";
-				}
-			}
-            else if ($key == "broker_module") {
+			} else if ($key == "broker_module") {
 				foreach ($nagios["broker_module"] as $kBrm => $vBrm)
 					if ( $vBrm["broker_module"] != NULL )
 						$str .= $key."=".$vBrm["broker_module"]."\n";
